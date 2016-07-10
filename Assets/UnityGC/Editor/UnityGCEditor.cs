0736937bc2b7116b627f54657cccfc65d7bc8f93
Seibe TAKAHASHI <t@seibe.jp>
2016-07-10 23:10:50 +0900

    Add UnityGCEditor

---
+++ Assets/UnityGC/Editor/UnityGCEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace GameCanvas
+{
+    namespace Editor
+    {
+        public class GameCanvasAssetProcessor : AssetPostprocessor
+        {
+            void OnPreprocessTexture()
+            {
+                // インポートした画像を、スクリプトから読み込み可能にします
+                TextureImporter textureImporter = (TextureImporter)assetImporter;
+                textureImporter.textureType = TextureImporterType.Advanced;
+                textureImporter.textureFormat = TextureImporterFormat.RGB24;
+                textureImporter.isReadable = true;
+            }
+        }
+    }
+}
