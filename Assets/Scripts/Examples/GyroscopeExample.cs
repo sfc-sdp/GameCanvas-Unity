761e100d05fd074f2ac855989d0e4670ccca84a1
Seibe TAKAHASHI <t@seibe.jp>
2016-08-30 21:18:44 +0900

    Add GyroscopeExample

---
+++ Assets/Scripts/Examples/GyroscopeExample.cs
@@ -0,0 +1,53 @@
+﻿using GameCanvas;
+
+public class GyroscopeExample : GameBase
+{
+    const int NUM_BALL = 3;
+
+    float[] ballX;
+    float[] ballY;
+    float[] ballDegree;
+
+    override public void Start()
+    {
+        // ジャイロスコープを有効化します
+        gc.enabledGyro = true;
+
+        // ボール3つ分の変数を用意します
+        ballX = new float[NUM_BALL];
+        ballY = new float[NUM_BALL];
+        ballDegree = new float[NUM_BALL];
+
+        // 画面左上にボールを並べます
+        for (int i = 0; i < NUM_BALL; ++i)
+        {
+            ballX[i] = 48 + i * 64;
+            ballY[i] = 48;
+            ballDegree[i] = 0;
+        }
+    }
+
+    override public void Calc()
+    {
+        // ボールの回転角度にジャイロスコープの値を加算します
+        ballDegree[0] += gc.gyroX;
+        ballDegree[1] += gc.gyroY;
+        ballDegree[2] += gc.gyroZ;
+    }
+
+    override public void Draw()
+    {
+        // 画面を白で塗りつぶします
+        gc.ClearScreen();
+
+        // 背景画像を描画します
+        gc.SetColor(1f, 1f, 1f);
+        gc.DrawImage(2, 0, 0);
+
+        // ボールを3つ描画します
+        for (int i = 0; i < NUM_BALL; ++i)
+        {
+            gc.DrawRotatedImage(i % 2, ballX[i], ballY[i], ballDegree[i], 12, 12);
+        }
+    }
+}
