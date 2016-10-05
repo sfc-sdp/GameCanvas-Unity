88a711d0ae5c6579c892de9d54945092db41d1dd
Seibe TAKAHASHI <t@seibe.jp>
2016-10-06 01:23:09 +0900

    Add ParticleExample

---
+++ Assets/Scripts/Examples/ParticleExample.cs
@@ -0,0 +1,70 @@
+﻿/**
+ * GameCanvas Particle Example
+ * 
+ * Copyright (c) 2016 Seibe TAKAHASHI
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+using GameCanvas;
+
+public class ParticleExample : GameBase
+{
+    int degree;
+
+    override public void Start()
+    {
+        // ゲーム解像度を 320x320 に設定します
+        gc.SetResolution(320, 320);
+
+        // 回転角を0にリセットします
+        degree = 0;
+    }
+
+    override public void Calc()
+    {
+        // 回転角を3度ずつ増やします
+        degree += 3;
+
+        // 360度以上になったら巻き戻します
+        if (degree >= 360)
+        {
+            degree -= 360;
+        }
+    }
+
+    override public void Draw()
+    {
+        // 画面を白で塗りつぶします
+        gc.ClearScreen();
+
+        // 背景（中抜きされた円）を描画します
+        int circleX, circleY;
+        for (var x = 0; x < 11; ++x)
+        {
+            for (var y = 0; y < 11; ++y)
+            {
+                circleX = x * 32;
+                circleY = y * 32;
+
+                gc.SetColor(0.9f, 0.9f, 0.9f);
+                gc.DrawCircle(circleX, circleY, 20);
+            }
+        }
+
+        // パーティクル（塗りつぶされた円）を描画します
+        for (var x = 0; x < 11; ++x)
+        {
+            for (var y = 0; y < 11; ++y)
+            {
+                var localDegree = degree + 30 * (x + y);
+                circleX = x * 32 + (int)(20 * gc.Cos(localDegree));
+                circleY = y * 32 + (int)(20 * gc.Sin(localDegree));
+
+                var lightness = (localDegree % 360) / 1080f;
+                gc.SetColor(lightness, lightness, lightness);
+                gc.FillCircle(circleX, circleY, 6);
+            }
+        }
+    }
+}
