35aca8852bcc69ee95e8436033e39a0dcc2e58ab
Seibe TAKAHASHI <t@seibe.jp>
2017-01-15 22:59:00 +0900

    Release v1.2.3

--- Assets/Plugins/UnityGC/Scripts/GameCanvasEnv.cs
+++ Assets/Plugins/UnityGC/Scripts/GameCanvasEnv.cs
@@ -18,7 +18,7 @@ namespace GameCanvas
         /// <summary>
         /// GameCanvas のバージョン情報
         /// </summary>
-        public const string Version = "v1.2.2";
+        public const string Version = "v1.2.3";
 
         /// <summary>
         /// GameCanvas API のバージョン情報
