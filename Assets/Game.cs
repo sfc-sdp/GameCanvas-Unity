17c7cc804c88803b7ce27d0e3081a11ed6a570f9
Seibe TAKAHASHI <t@seibe.jp>
2017-09-26 23:12:11 +0900

    サンプルコードの簡略化

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,12 +5,11 @@
 /// </summary>
 public sealed class Game : GameBase
 {
-    // ここに変数の宣言を書きます
-    int time = 0;
+    // 変数の宣言
+    int sec = 0;
 
     /// <summary>
-    /// この関数は起動時に１回だけ呼ばれます。
-    /// ここに初期化処理を書きます。
+    /// 初期化処理
     /// </summary>
     public override void InitGame()
     {
@@ -19,18 +18,16 @@ public sealed class Game : GameBase
     }
 
     /// <summary>
-    /// この関数は毎フレーム呼ばれます。1フレームは1/30秒です。設定で変更もできます。
-    /// ここに動きなどの更新処理を書きます。
+    /// 動きなどの更新処理
     /// </summary>
     public override void UpdateGame()
     {
         // 起動からの経過時間を取得します
-        time = (int)gc.TimeSinceStartup;
+        sec = (int)gc.TimeSinceStartup;
     }
 
     /// <summary>
-    /// この関数は毎フレーム呼ばれます。ここに描画の処理をまとめます。
-    /// 奥に描きたいものは最初に、手前に描きたいものは最後に記述します。
+    /// 描画の処理
     /// </summary>
     public override void DrawGame()
     {
@@ -42,9 +39,9 @@ public sealed class Game : GameBase
 
         // 黒の文字を描画します
         gc.SetColor(0, 0, 0);
-        gc.SetFontSize(50);
+        gc.SetFontSize(48);
         gc.DrawString("この文字と青空の画像が", 40, 160);
-        gc.DrawString("見えていれば成功です", 40, 280);
-        gc.DrawRightString($"{time}s", 630, 10);
+        gc.DrawString("見えていれば成功です", 40, 270);
+        gc.DrawRightString($"{sec}s", 630, 10);
     }
 }
