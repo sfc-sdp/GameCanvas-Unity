13dd623f65d0c6da5575fe86de5d786d44e581e1
Seibe TAKAHASHI <t@seibe.jp>
2017-11-23 01:17:48 +0900

    描画テストシーン追加

---
+++ Assets/Plugins/GameCanvas/Tests/DrawLine.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GameCanvas.Tests
+{
+    public sealed class DrawLine : GameBase
+    {
+        public override void DrawGame()
+        {
+            gc.ClearScreen();
+
+            var y = 0;
+            for (var i = 0; i < 10; ++i)
+            {
+                y += i * 20;
+                gc.SetColor(i * 6, i * 12, i * 25);
+                gc.DrawLine(i * 20, y, 100 + i * 40, y + i * 30);
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/GameCanvas/Tests/DrawRect.cs
@@ -0,0 +1,20 @@
+﻿
+namespace GameCanvas.Tests
+{
+    public sealed class DrawRect : GameBase
+    {
+        public override void DrawGame()
+        {
+            gc.ClearScreen();
+
+            var y = 0;
+            for (var i = 0; i < 10; ++i)
+            {
+                y += i * 20;
+                gc.SetColor(i * 6, i * 12, i * 25);
+                gc.SetFontSize(i * 15 + 1);
+                gc.FillRect(0, y, i * 50 + 50, i * 15 + 15);
+            }
+        }
+    }
+}
