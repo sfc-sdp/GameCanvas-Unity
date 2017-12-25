de65451914264bc871a5963201b2136b7d644fcb
Seibe TAKAHASHI <t@seibe.jp>
2017-12-25 17:15:19 +0900

    パッケージ出力ショートカットの追加

---
+++ Assets/Plugins/GameCanvas/Scripts/Exporter.cs
@@ -0,0 +1,31 @@
+﻿/*------------------------------------------------------------*/
+// <summary>GameCanvas for Unity</summary>
+// <author>Seibe TAKAHASHI</author>
+// <remarks>
+// (c) 2015-2017 Smart Device Programming.
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// </remarks>
+/*------------------------------------------------------------*/
+
+#if UNITY_EDITOR
+namespace GameCanvas.Editor
+{
+    using UnityEditor;
+
+    public sealed class Exporter
+    {
+        //----------------------------------------------------------
+        #region パブリック関数
+        //----------------------------------------------------------
+
+        [MenuItem("GameCanvas/パッケージを出力する")]
+        public static void ExportPackage()
+        {
+            EditorApplication.ExecuteMenuItem("Assets/Export Package...");
+        }
+
+        #endregion
+    }
+}
+#endif //UNITY_EDITOR
