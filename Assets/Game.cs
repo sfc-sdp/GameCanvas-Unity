dac3f9dfe2d3be5f893b19b50683d752af0aa0d7
Seibe TAKAHASHI <t@seibe.jp>
2018-04-07 15:10:11 +0900

    旧実装を優先する

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -7,35 +7,43 @@ using Sequence = System.Collections.IEnumerator;
 /// </summary>
 public sealed class Game : GameBase
 {
+    // 変数の宣言
+    int sec = 0;
+
     /// <summary>
-    /// ゲームシーケンス
+    /// 初期化処理
     /// </summary>
-    public override Sequence Entry()
+    public override void InitGame()
     {
         // キャンバスの大きさを設定します
         gc.SetResolution(720, 1280);
-        
-        // 無限に繰り返します
-        while (true)
-        {
-            // 起動からの経過時間を取得します
-            var sec = (int)gc.TimeSinceStartup;
+    }
 
-            // 画面を白で塗りつぶします
-            gc.ClearScreen();
+    /// <summary>
+    /// 動きなどの更新処理
+    /// </summary>
+    public override void UpdateGame()
+    {
+        // 起動からの経過時間を取得します
+        sec = (int)gc.TimeSinceStartup;
+    }
 
-            // 0番の画像を描画します
-            gc.DrawImage(0, 0, 0);
+    /// <summary>
+    /// 描画の処理
+    /// </summary>
+    public override void DrawGame()
+    {
+        // 画面を白で塗りつぶします
+        gc.ClearScreen();
 
-            // 黒の文字を描画します
-            gc.SetColor(0, 0, 0);
-            gc.SetFontSize(48);
-            gc.DrawString("この文字と青空の画像が", 40, 160);
-            gc.DrawString("見えていれば成功です", 40, 270);
-            gc.DrawRightString($"{sec}s", 630, 10);
+        // 0番の画像を描画します
+        gc.DrawImage(0, 0, 0);
 
-            // 次のフレームになるまで待ちます
-            yield return gc.WaitForNextFrame;
-        }
+        // 黒の文字を描画します
+        gc.SetColor(0, 0, 0);
+        gc.SetFontSize(48);
+        gc.DrawString("この文字と青空の画像が", 40, 160);
+        gc.DrawString("見えていれば成功です", 40, 270);
+        gc.DrawRightString($"{sec}s", 630, 10);
     }
 }
