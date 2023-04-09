a2fdcc90ce2efcadf6a146e580a23328bd680e21
Seibe TAKAHASHI <t@seibe.jp>
2023-04-09 16:44:55 +0900

    chore: サンプルコードの整理とパッケージ統合

--- Assets/GameCanvas/AutoGeneratedScripts/GcImage.cs
+++ Assets/GameCanvas/AutoGeneratedScripts/GcImage.cs
@@ -13,9 +13,10 @@ namespace GameCanvas
 {
     public readonly partial struct GcImage : System.IEquatable<GcImage>
     {
-        internal const int __Length__ = 3;
+        internal const int __Length__ = 4;
         public static readonly GcImage BallRed = new GcImage("BallRed", 24, 24);
         public static readonly GcImage BallYellow = new GcImage("BallYellow", 24, 24);
         public static readonly GcImage BlueSky = new GcImage("BlueSky", 640, 480);
+        public static readonly GcImage MapPin = new GcImage("MapPin", 50, 64);
     }
 }
